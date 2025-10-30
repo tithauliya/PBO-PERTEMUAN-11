b05626e1f9c0ed96f943e5e1ea494e643681e1d7
tithauliya <tithauliya21@gmail.com>
2025-10-30 10:39:00 +0700

    Add files via upload

---
+++ PERTEMUAN11/build/generated-sources/ap-source-output/pertemuan11/Tokoroti_.java
@@ -0,0 +1,16 @@
+package pertemuan11;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-10-29T13:21:39", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Tokoroti.class)
+public class Tokoroti_ { 
+
+    public static volatile SingularAttribute<Tokoroti, String> namaKue;
+    public static volatile SingularAttribute<Tokoroti, Integer> harga;
+    public static volatile SingularAttribute<Tokoroti, String> idKue;
+    public static volatile SingularAttribute<Tokoroti, Integer> stock;
+
+}
